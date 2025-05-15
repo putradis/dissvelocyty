a962c47934da83875adaa66123a72a08a180e081
putradis <informatikaputra27@gmail.com>
2025-05-15 14:12:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 wa import streamlit as st
 
 st.title("Selamat datang di website disvelocyty")
-st.image("https://github.com/putradis/dissvelocyty/blob/main/view/IMG_1404.jpeg", width=200) 
+st.image("view/IMG_1404.jpeg", width=200) 
 st.write("\n")
 st.subheader("Mr. Muhammad Yudistira Putra Priatman") 
 st.write("Mari kita belajar velocyty bersama Mr.velocyty")
